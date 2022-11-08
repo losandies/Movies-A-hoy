d08bc42e74dcbb441394b712c104b9abd5e2d50b
Brandon Newsome <brandon.i.newsome@gmail.com>
2022-11-07 23:35:41 -0500

    heroku prep

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import tw from 'twin.macro';
 import { useMediaQuery } from 'react-responsive';
 import { SCREENS } from '../../responsive/screens';
 import { HiSearch } from 'react-icons/hi';
-import NavItems from '../navbar/NavItems';
+import NavItems from '../navbar/NavItems.jsx';
 
 const NavContainer = styled.div`
 	min-height: 68px;
