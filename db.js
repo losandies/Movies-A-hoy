50c1ebf4b669674d9cef0b6b01391c1656252a26
Brandon Newsome <brandon.i.newsome@gmail.com>
2022-11-08 20:52:49 -0500

    tweaked ssl requirements:

--- db.js
+++ db.js
@@ -7,7 +7,10 @@ const devConfig = {
 	host: process.env.PG_HOST,
 	port: process.env.PG_PORT,
 	database: process.env.PG_DATABASE,
-	ssl: true,
+	ssl: {
+		required: true,
+		rejectUnauthorized: false,
+	},
 };
 
 const prodConfig = {
