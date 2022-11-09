b9bf7e47785368f8e005dbe387cd192de307ddec
Brandon Newsome <brandon.i.newsome@gmail.com>
2022-11-08 19:48:11 -0500

    prep db for heroku

--- db.js
+++ db.js
@@ -1,11 +1,20 @@
 const { Client } = require('pg');
+require('dotenv').config();
 
-const db = new Client({
-	user: 'sir9eleven',
-	password: 'sir9eleven',
-	host: 'localhost',
-	port: 5432,
-	database: 'moviesahoy',
-});
+const devConfig = {
+	user: process.env.PG_USER,
+	password: process.env.PG_PASSWORD,
+	host: process.env.PG_HOST,
+	port: process.env.PG_PORT,
+	database: process.env.PG_DATABASE,
+};
+
+const prodConfig = {
+	connectionString: process.env.DATABASE_URL,
+};
+
+const db = new Client(
+	process.env.NODE_ENV === 'production' ? prodConfig : devConfig
+);
 
 module.exports = db;
