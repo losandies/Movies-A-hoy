2c5dcf3dbf96eaf66522d68a21e54e91865bfbef
Brandon Newsome <brandon.i.newsome@gmail.com>
2022-11-08 20:20:05 -0500

    imported path module

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const db = require('./db');
 const colors = require('colors');
+const path = require('path');
 require('dotenv').config();
 
 const app = express();
